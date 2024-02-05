1b3b0d1e840c72e692305208776e9101528bd971
SalmY001 <80605132+SalmY001@users.noreply.github.com>
2024-02-05 17:58:15 +0000

    Add files via upload

---
+++ timer_countdown.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+try:
+    select_time = int(input("Enter time required in seconds: "))
+except ValueError:
+    print("Try again, enter integer numbers only.")
+else:
+    for i in range(select_time, 0, -1):
+        seconds = i % 60
+        mins = int(i / 60) % 60
+        hours = int(i / 3600)
+        print(f"{hours:02}: {mins:02}: {seconds:02}", end="\r")
+        sleep(1)
+    print("Time is over!")
